3dc0e9552a2c87c837dda744832bc62a7ac925c2
Tintgire <boidinhugo14@gmail.com>
2022-12-26 16:18:43 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './components/pages/Home/index'
+import Home from './components/pages/Home'
+import Logement from './components/pages/Logement'
+import About from './components/pages/About'
+import Error from './components/pages/Error'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/logement/:id" element={<Logement />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </div>
   )
 }
 
