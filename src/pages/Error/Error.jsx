77ba5e6a7a69b5899edf9f515e6ba7ecdaa7ec21
Tintgire <boidinhugo14@gmail.com>
2022-12-28 17:59:41 +0100

    error

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,3 +1,20 @@
+import './Error.scss'
+import Header from '../../components/header/Header'
+import { Link } from 'react-router-dom'
+
 export default function Error() {
-  return <div>Error</div>
+  return (
+    <div>
+      <Header />
+      <h1 className="error error__tittle">404</h1>
+      <h2 className="error error__second">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <h3>
+        <Link className="error" to="/">
+          <p className="error__third">Retourner sur la page d’accueil</p>
+        </Link>
+      </h3>
+    </div>
+  )
 }
