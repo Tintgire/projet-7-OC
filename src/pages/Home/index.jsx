81581454dcfb7ca36f3b2aa9c3c385fb922932da
Tintgire <boidinhugo14@gmail.com>
2022-12-28 13:19:27 +0100

    import footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import './index.scss'
 import Header from '../../components/header'
 import HomeBanner from '../../components/homebanner'
+import Footer from '../../components/footer'
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HomeBanner />
+      <Footer />
     </div>
   )
 }
