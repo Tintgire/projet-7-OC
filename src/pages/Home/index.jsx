443dbd41dddc6fb518ede20c289dbd8bf2534fb6
Tintgire <boidinhugo14@gmail.com>
2022-12-28 12:40:03 +0100

    insert image + filtre

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,8 +6,14 @@ export default function Home() {
   return (
     <div className="home">
       <Header />
-      <div>
-        <img className="imghome" src={IMGHOME} alt="IMGHOME" />
+      <div className="homeBanner">
+        <img
+          className="homeBanner__imghome background"
+          src={IMGHOME}
+          alt="IMGHOME"
+        />
+        <div className="homeBanner__overlay background"></div>
+        <h1>Chez vous, partout et ailleurs</h1>
       </div>
     </div>
   )
