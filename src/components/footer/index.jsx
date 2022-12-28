608d12012d763950a1a3bf15b38b2a5248c8e6dd
Tintgire <boidinhugo14@gmail.com>
2022-12-28 14:00:33 +0100

    function footer done

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,11 @@
 import './index.scss'
+import LogoFooter from '../../assets/LOGO-footer.png'
 
 export default function Footer() {
-  return <footer></footer>
+  return (
+    <footer>
+      <img className="logo-footer" src={LogoFooter} alt="Logo footer" />
+      <p className="kasarights">© 2020 Kasa. All rights reserved</p>
+    </footer>
+  )
 }
