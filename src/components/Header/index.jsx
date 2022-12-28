d1d33301855fa47f2ddc11b5a51a4aa8567a9e6b
Tintgire <boidinhugo14@gmail.com>
2022-12-28 10:34:26 +0100

    corection div

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,15 @@
 import './index.scss'
 import LOGO from '../../assets/LOGO.png'
 import Navbar from '../navbar'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className="container">
-      <img className="logo" src={LOGO} alt="logo" />
+    <header>
+      <Link className="" to="/">
+        <img className="logo" src={LOGO} alt="logo" />
+      </Link>
       <Navbar />
-    </div>
+    </header>
   )
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>
-      <nav className="navbar">
-        <Link className="linkacceuil" to="/">
-          Accueil
-        </Link>
-        <Link className="linkabout" to="/about">
-          A Propos
-        </Link>
-      </nav>
-    </div>
+    <nav>
+      <Link className="linkacceuil link-header" to="/">
+        Accueil
+      </Link>
+      <Link className="linkabout link-header" to="/about">
+        A Propos
+      </Link>
+    </nav>
   )
 }
