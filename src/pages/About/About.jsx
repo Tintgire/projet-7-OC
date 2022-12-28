450f9dfb8ad852a392e3bd601f01fae46de703b2
Tintgire <boidinhugo14@gmail.com>
2022-12-28 20:38:52 +0100

    up

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,13 @@
+import Header from '../../components/header/Header'
+import Banner from '../../components/banner/Banner'
+import Footer from '../../components/footer/Footer'
+
 export default function About() {
-  return <div>About</div>
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  )
 }
