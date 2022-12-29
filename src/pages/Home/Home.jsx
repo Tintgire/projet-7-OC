ff4a9df7697c4a0662892cf9c22c88a3d2c741d4
Tintgire <boidinhugo14@gmail.com>
2022-12-29 12:38:46 +0100

    changement nom props

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
 import Header from '../../components/header/Header'
 import Banner from '../../components/banner/Banner'
 import Footer from '../../components/footer/Footer'
+import IMGHOME from '../../assets/IMGHOME.png'
 // import image
 
 export default function Home() {
-  const textparagraphe = 'Chez vous, partout et ailleurs'
+  const textHOME = 'Chez vous, partout et ailleurs'
+  const altHOME = 'image de Koh avec une vue mer'
   return (
     <div>
       <Header />
-      <Banner text={textparagraphe} />
+      <Banner text={textHOME} src={IMGHOME} alt={altHOME} />
       <Footer />
     </div>
   )
