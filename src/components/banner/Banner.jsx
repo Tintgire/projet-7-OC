a78760105055838d6d35590def53fbac9b364cd4
Tintgire <boidinhugo14@gmail.com>
2022-12-29 12:39:00 +0100

    création props pour a propos

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,11 +1,14 @@
 import './Banner.scss'
-import IMGHOME from '../../assets/IMGHOME.png'
 
 export default function banner(props) {
   return (
     /* render Chez vous, partout et ailleurs*/
     <div className="banner">
-      <img className="banner__imghome background" src={IMGHOME} alt="IMGHOME" />
+      <img
+        className="banner__imghome background"
+        src={props.src}
+        alt={props.alt}
+      />
       <div className="banner__overlay background"></div>
       <h1>{props.text}</h1>
     </div>
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,15 @@
 import Header from '../../components/header/Header'
 import Banner from '../../components/banner/Banner'
 import Footer from '../../components/footer/Footer'
+import IMGABOUT from '../../assets/IMGABOUT.png'
 
 export default function About() {
+  const textHOME = ''
+  const altHOME = 'image de Koh avec une vue mer'
   return (
     <div>
       <Header />
-      <Banner />
+      <Banner text={textHOME} src={IMGABOUT} alt={altHOME} />
       <Footer />
     </div>
   )
