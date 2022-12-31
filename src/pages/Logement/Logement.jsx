7fa98da4c10d761b2e849ac99f64ff115090d561
Tintgire <boidinhugo14@gmail.com>
2022-12-31 19:49:50 +0100

    ajout carrousel

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
+import Carrousel from '../../components/carrousel/Carrousel'
 
 export default function Logement() {
   return (
     <div>
       <Header />
+      <Carrousel />
       <Footer />
     </div>
   )
