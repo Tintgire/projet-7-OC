27f926983a94cb071d3580f8ce3f209766740d73
Tintgire <boidinhugo14@gmail.com>
2023-01-02 19:50:41 +0100

    placement du main

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -6,12 +6,12 @@ import Carrousel from '../../components/carrousel/Carrousel'
 import Collapse from '../../components/collapse/Collapse'
 import CardItems from '../../DataBase/logement.js'
 import Button from '../../components/button/Button'
+import Star from '../../components/star/Star'
 
 export default function Logement() {
   const { id } = useParams()
   const foundItem = CardItems.find((item) => item.id === id)
   const tags = foundItem.tags
-  console.log(tags)
 
   const listequipments = foundItem.equipments.map((item, i) => (
     <li key={i}>{item}</li>
@@ -21,12 +21,27 @@ export default function Logement() {
     <div className="logement">
       <Header />
       <Carrousel />
-      <h3 className="logement-title">{foundItem.title}</h3>
-      <h3 className="logement-location">{foundItem.location}</h3>
-      <div className="logement-all-button">
-        {tags.map((tag, i) => (
-          <Button key={i} tags={tag} />
-        ))}
+      <div className="main">
+        <div>
+          <h3 className="logement-title">{foundItem.title}</h3>
+          <h3 className="logement-location">{foundItem.location}</h3>
+          <div className="logement-all-button">
+            {tags.map((tag, i) => (
+              <Button key={i} tags={tag} />
+            ))}
+          </div>
+        </div>
+        <div className="countainer-img-star">
+          <div className="logement-picture-name">
+            <h3 className="logement-name">{foundItem.host.name}</h3>
+            <img
+              className="logement-picture"
+              src={foundItem.host.picture}
+              alt="img de la personne"
+            />
+          </div>
+          <Star rating={foundItem.rating} />
+        </div>
       </div>
       <div className="collapse-logement">
         <Collapse title="Description" texte={foundItem.description} />
