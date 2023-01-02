3133a4f890d54906e76b1bb2e3a2153754f01a48
Tintgire <boidinhugo14@gmail.com>
2023-01-02 19:49:54 +0100

    composent star

--- src/components/star/Star.jsx
+++ src/components/star/Star.jsx
@@ -1,3 +1,18 @@
+import Starcolor from '../../assets/star.png'
+import Starnocolor from '../../assets/starnocolor.png'
+
 export default function Star(props) {
-  return <div>Star</div>
+  const MaxStar = 5
+  const nbStar = props.rating
+  const stars = []
+
+  for (let i = 0; i < MaxStar; i++) {
+    if (i < nbStar) {
+      stars.push(<img key={i} src={Starcolor} alt="étoile coloré" />)
+    } else {
+      stars.push(<img key={i} src={Starnocolor} alt="étoile non coloré" />)
+    }
+  }
+
+  return <div className="countainer-star">{stars}</div>
 }
