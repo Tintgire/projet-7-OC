f58a09e2c8aa91533a5fb724334147f96a65317f
Tintgire <boidinhugo14@gmail.com>
2023-01-03 22:25:40 +0100

    rajout class star

--- src/components/star/Star.jsx
+++ src/components/star/Star.jsx
@@ -1,5 +1,6 @@
 import Starcolor from '../../assets/star.png'
 import Starnocolor from '../../assets/starnocolor.png'
+import './Star.scss'
 
 export default function Star(props) {
   const MaxStar = 5
@@ -8,9 +9,18 @@ export default function Star(props) {
 
   for (let i = 0; i < MaxStar; i++) {
     if (i < nbStar) {
-      stars.push(<img key={i} src={Starcolor} alt="étoile coloré" />)
+      stars.push(
+        <img key={i} className="star" src={Starcolor} alt="étoile coloré" />
+      )
     } else {
-      stars.push(<img key={i} src={Starnocolor} alt="étoile non coloré" />)
+      stars.push(
+        <img
+          key={i}
+          className="star"
+          src={Starnocolor}
+          alt="étoile non coloré"
+        />
+      )
     }
   }
 
